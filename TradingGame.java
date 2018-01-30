6048166eca303816399dea32f9fc6f60e99bf366
Ryan Cudd <ryancudd@casady.org>
2018-01-30 09:12:37 -0600

    first changes

--- TradingGame.java
+++ TradingGame.java
@@ -2,8 +2,33 @@ import java.text.*;
 import java.util.*;
 
 class TradingGame{
-    public static void main(String[] args){
+    static final int NUMBER_OF_DAYS = 10;
+    static final double BASE_PRICE = 10;
+    static final double VARIATION = 5;
+    static final double INITIAL_CASH = 100;
 
+    static double cash = INITIAL_CASH;
+    static int appleInventory = 0;
+    static int pearInventory = 0;
+    static double applePrice, pearPrice;
+
+    public static void main(String[] args){
+        for (int day = 1; day <= NUMBER_OF_DAYS; day++){
+            applePrice = computePrice(BASE_PRICE, VARIATION);
+            pearPrice = computePrice(BASE_PRICE, VARIATION);
+            System.out.println("Day: " + day + " out of 10");
+            int choice;
+            do{
+                printMenu()
+                choice = getChoice();
+                switch (choice){
+                    case 1: // Print cash balance and inventory
+                        System.out.println("Cash: " + currencyFormatter(cash));
+                        System.out.println("Apple inventory: " + appleInventory);
+                        System.out.println("Pear inventory: " + pearInventory);
+                }
+            }
+        }
     }
 
     public static void printMenu(){
@@ -39,6 +64,6 @@ class TradingGame{
     }
 
     public static boolean buyPears(int amount){
-        
+
     }
 }
