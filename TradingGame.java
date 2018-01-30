cd981b21378a2e74bae5e3773756a57f99b09bc4
Heath Stanfield <33321092+heathstanfield@users.noreply.github.com>
2018-01-30 09:08:18 -0600

    Changes

--- TradingGame.java
+++ TradingGame.java
@@ -7,6 +7,13 @@ class TradingGame{
     }
 
     public static void printMenu(){
+      System.out.println("1. Print cash balance and inventory");
+      System.out.println("2. Print today's prices");
+      System.out.println("3. Buy apples");
+      System.out.println("4. Sell Apples");
+      System.out.println("5. Buy pears");
+      System.out.println("6. Sell pears");
+      Sysetm.out.println("7. I am done for today");
 
     }
 
@@ -15,7 +22,8 @@ class TradingGame{
     }
 
     public static String currency Formatter(double amount){
-
+      DecimalFormat myFormatter = new DecimalFormat("$###,###.00");
+      return myFormatter.format(amount);
     }
 
     public static double computePrice(double basePrice, double variation){
@@ -39,6 +47,6 @@ class TradingGame{
     }
 
     public static boolean buyPears(int amount){
-        
+
     }
 }
