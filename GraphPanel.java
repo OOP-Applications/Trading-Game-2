5b0405e23ffe964e51524636cde77bbe2214ae46
PrestonPiercey <prestonpiercey@casady.org>
2018-02-14 09:49:42 -0600

    constructor beginning

--- GraphPanel.java
+++ GraphPanel.java
@@ -6,34 +6,45 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 public class GraphPanel extends Application{
-    @Override public void start(Stage stage, list){
+
+    @Override public void start(Stage stage){
+
         stage.setTitle("Trading Game Graph");
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Price");
+
         xAxis.setLabel("Day of Trading");
+
         final LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
         lineChart.setTitle("Trading Game Graph");
-        final XYChart.Series<Number,Number> series = new XYChart.Series<>();
-        series.setName("My portfolio");
-        series.getData().add(new XYChart.Data<>(1, NYappleprices.get(0)));
-        series.getData().add(new XYChart.Data<>(2, NYappleprices.get(1)));
-        series.getData().add(new XYChart.Data<>(3, NYappleprices.get(2)));
-        series.getData().add(new XYChart.Data<>(4, NYappleprices.get(3)));
-        series.getData().add(new XYChart.Data<>(5, NYappleprices.get(4)));
-        series.getData().add(new XYChart.Data<>(6, NYappleprices.get(5)));
-        series.getData().add(new XYChart.Data<>(7, NYappleprices.get(6)));
-        series.getData().add(new XYChart.Data<>(8, NYappleprices.get(7)));
-        series.getData().add(new XYChart.Data<>(9, NYappleprices.get(8)));
-        series.getData().add(new XYChart.Data<>(10, NYappleprices.get(9)));
+
+        final XYChart.Series series = new XYChart.Series<>();
+
+        series.setName("New York Apple Prices");
+
+        series.getData().add(new XYChart.Data<>(1, 20));
+        series.getData().add(new XYChart.Data<>(2, 14));
+        series.getData().add(new XYChart.Data<>(3, 15));
+        series.getData().add(new XYChart.Data<>(4, 24));
+        series.getData().add(new XYChart.Data<>(5, 34));
+        series.getData().add(new XYChart.Data<>(6, 36));
+        series.getData().add(new XYChart.Data<>(7, 22));
+        series.getData().add(new XYChart.Data<>(8, 45));
+        series.getData().add(new XYChart.Data<>(9, 43));
+        series.getData().add(new XYChart.Data<>(10, 17));
 
         Scene scene = new Scene(lineChart, 800, 600);
+
         lineChart.getData().add(series);
 
         stage.setScene(scene);
         stage.show();
     }
-
+    /*
+    public GraphPanel(){
+        launch();
+    }
+    */
     public static void main(String[] args) {
         launch(args);
     }
