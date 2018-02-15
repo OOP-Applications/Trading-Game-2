6a0d88c132e7721cc58f28e1bb29cfce5c9dcb4c
Lukas <lukasschneider@casady.org>
2018-02-15 08:50:00 -0600

    Commenting and Redo of the Add Cap

--- TradingGame.java
+++ TradingGame.java
@@ -1,4 +1,4 @@
-   import java.text.*;
+import java.text.*;
 import java.util.*;
 
 class TradingGame{
@@ -10,13 +10,15 @@ class TradingGame{
 
     static double cash = INITIAL_CASH;
 
-
+    //This creates the hashtables to be used to keep track of prices and inventories.
     static final Hashtable<String, Double> prices = new Hashtable<String, Double> ();
     static final Hashtable<String, Integer> inventories = new Hashtable<String, Integer> ();
 
 
     public static void main(String[] args){
+        // Creates the list to contain the fruits
         List<String> fruitlist = new ArrayList<>();
+        //Adds the pears and apples and their base inventory at 0.
         fruitlist.add("pear");
         fruitlist.add("apple");
         inventories.put("apple", 0);
@@ -25,6 +27,7 @@ class TradingGame{
             int listlen = fruitlist.size();
             for (int i = 0; i < listlen; i++){
               String fruiter = fruitlist.get(i);
+              //Creates the prices daily for each fruit in the fruitlist
               double priceloop2 = computePrice(BASE_PRICE, VARIATION);
               prices.put(fruiter, priceloop2);
             }
@@ -37,11 +40,13 @@ class TradingGame{
                 switch (choice){
                     case 1: // Print cash balance and inventory
                         int listlen3 = fruitlist.size();
+                        //Prints the inventory of each fruit and the current amount of cash.
                         for (int i = 0; i < listlen3; i++){
                             String fruiter3 = fruitlist.get(i);
                             String priceloop3 = currencyFormatter(prices.get(fruiter3));
                             System.out.println(fruiter3 + " inventory: " + inventories.get(fruiter3));
                         }
+                        //Prints how much cash
                         System.out.println("Cash: " + currencyFormatter(cash));
 
                         break;
@@ -58,13 +63,14 @@ class TradingGame{
                           System.out.println("These are the available fruits " + fruitlist);
                           Scanner buykeyboard = new Scanner(System.in);
                           String buyfruit = buykeyboard.nextLine();
-
+                          //Checks to make sure the fruit is on the market.
                           int listlen6 = fruitlist.size();
                           if(!checkFruits2(listlen6, fruitlist, buyfruit)){
                             System.out.println("That fruit is not on the market");
                             break;
                           }
                           else{
+                            //Checks to make sure the user has enough money
                             amount = getQuantity(buyfruit, "buy");
                             if (!buyFruits(amount, buyfruit)) {
                                 System.out.println("You don't have enough money.");
@@ -80,11 +86,13 @@ class TradingGame{
                         Scanner sellkeyboard = new Scanner(System.in);
                         String sellfruit = sellkeyboard.nextLine();
                         int listlen7 = fruitlist.size();
+                        //Checks to see if the fruit is on the market.
                         if(!checkFruits2(listlen7, fruitlist, sellfruit)){
                           System.out.println("That fruit is not on the market");
                           break;
                         }
                         else{
+                            //Checks to make sure you have enough fruit to sell.
                           amount = getQuantity(sellfruit, "sell");
                           if (!sellFruits(amount, sellfruit)){
                               System.out.println("You don't have enough " + sellfruit);
@@ -94,6 +102,12 @@ class TradingGame{
                             break;
                         }
                     case 5: // Add Fruit
+                        int listlen4 = fruitlist.size();
+                        //Stops the method if there are too many fruits on the market already
+                        if(listlen4 == 10){
+                            System.out.println("There are too many fruits in the list");
+                            break;
+                        }
                         while (true){
                           System.out.println("What fruit do you want to add?");
                           System.out.println("These fruits are already being sold " + fruitlist);
@@ -104,6 +118,7 @@ class TradingGame{
                             fruitlist.add(newfruit);
                             inventories.put(newfruit, 0);
                             prices.put(newfruit, computePrice(BASE_PRICE, VARIATION));
+                            cash -= 10;
                             break;
                           }
                           System.out.println("Its not in the list, Try again: ");
@@ -157,6 +172,7 @@ class TradingGame{
       return keyboard.nextInt();
     }
     public static boolean sellFruits(int amount, String fruit){
+        // Function that gets the price and current inventory of the fruit and sells a specific number of them, then updating the variables, most importantly giving the user more cash.
         double price = prices.get(fruit);
         int inventory = inventories.get(fruit);
         if (amount > inventory) {
@@ -168,6 +184,7 @@ class TradingGame{
         return true;
     }
     public static boolean buyFruits(int amount, String fruit){
+        // Function that gets the price and current inventory of the fruit and buys a specific number of them, then updating the variables.
         double price = prices.get(fruit);
         int inventory = inventories.get(fruit);
         if (amount * price < cash) {
